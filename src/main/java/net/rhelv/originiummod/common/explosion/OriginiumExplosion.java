a310ea8d06bd543fb8119185b72f96c1e0e4f05f
Rhelvetican <bionicvnb@gmail.com>
2022-10-03 13:46:36 +0700

    Kanged from TechReborn lol II:

--- src/main/java/net/rhelv/originiummod/common/explosion/OriginiumExplosion.java
+++ src/main/java/net/rhelv/originiummod/common/explosion/OriginiumExplosion.java
@@ -5,8 +5,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import net.rhelv.originiummod.OriginiumMod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.apache.commons.lang3.time.StopWatch;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OriginiumExplosion extends Explosion {
     @NotNull
@@ -33,5 +40,62 @@ public class OriginiumExplosion extends Explosion {
     }
     public @Nullable LivingEntity getLivingBase() {
         return livingBase;
+    }
+    public void applyExplosion() {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        for (int tx = -radius; tx < radius + 1; tx++) {
+            for (int ty = -radius; ty < radius + 1; ty++) {
+                for (int tz = -radius; tz < radius + 1; tz++) {
+                    if (Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2) + Math.pow(tz, 2)) <= radius - 2) {
+                        BlockPos pos = center.add(tx, ty, tz);
+                        BlockState state = world.getBlockState(pos);
+                        Block block = state.getBlock();
+                        if (block != Blocks.BEDROCK && !state.isAir()) {
+                            block.onDestroyedByExplosion(world, pos, this);
+                            world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+                        }
+                    }
+                }
+            }
+        }
+        OriginiumMod.LOGGER.info("The explosion took" + watch + " to explode");
+    }
+
+    @Override
+    public void collectBlocksAndDamageEntities() {
+        applyExplosion();
+    }
+
+    @Override
+    public void affectWorld(boolean spawnParticles) {
+        applyExplosion();
+    }
+
+    @Override
+    public
+    @Nullable
+    LivingEntity getCausingEntity() {
+        return livingBase;
+    }
+
+    @Override
+    public List<BlockPos> getAffectedBlocks() {
+        List<BlockPos> poses = new ArrayList<>();
+        for (int tx = -radius; tx < radius + 1; tx++) {
+            for (int ty = -radius; ty < radius + 1; ty++) {
+                for (int tz = -radius; tz < radius + 1; tz++) {
+                    if (Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2) + Math.pow(tz, 2)) <= radius - 2) {
+                        BlockPos pos = center.add(tx, ty, tz);
+                        BlockState state = world.getBlockState(pos);
+                        Block block = state.getBlock();
+                        if (block != Blocks.BEDROCK && !state.isAir()) {
+                            poses.add(pos);
+                        }
+                    }
+                }
+            }
+        }
+        return poses;
     }
 }
