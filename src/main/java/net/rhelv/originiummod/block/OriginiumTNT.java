4330d82b43e537d010ba3040dda9f40f543e86d4
Rhelvetican <bionicvnb@gmail.com>
2022-10-05 08:20:11 +0700

    Planned rework

--- src/main/java/net/rhelv/originiummod/block/OriginiumTNT.java
+++ src/main/java/net/rhelv/originiummod/block/OriginiumTNT.java
@@ -1,21 +1,9 @@
 package net.rhelv.originiummod.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.TntBlock;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class OriginiumTNT extends TntBlock {
     public OriginiumTNT(Settings settings) {
         super(settings);
-    }
-    @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block sourceBlock, BlockPos sourcePos, boolean notify) {
-        if (world.isReceivingRedstonePower(pos)) {
-            primeTnt(world, pos);
-            world.removeBlock(pos, false);
-        }
-
     }
 }
