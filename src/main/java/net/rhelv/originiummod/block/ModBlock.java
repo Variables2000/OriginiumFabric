97f538194d7e1ed85e3886bf58ec57bb45929162
Rhelvetican <bionicvnb@gmail.com>
2022-10-12 19:09:04 +0700

    Minor changes.

--- src/main/java/net/rhelv/originiummod/block/ModBlock.java
+++ src/main/java/net/rhelv/originiummod/block/ModBlock.java
@@ -1,7 +1,9 @@
 package net.rhelv.originiummod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -9,6 +11,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import net.rhelv.originiummod.OriginiumMod;
+import net.rhelv.originiummod.item.ModItemGroup;
 
 public class ModBlock{
     public static void init() {
---
+++ src/main/java/net/rhelv/originiummod/entity/ModEntityType.java
@@ -0,0 +1,8 @@
+package net.rhelv.originiummod.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+
+public class ModEntityType<T extends Entity> {
+    public static EntityType<OriginiumTNTEntity> ORIGINIUM_TNT;
+}
